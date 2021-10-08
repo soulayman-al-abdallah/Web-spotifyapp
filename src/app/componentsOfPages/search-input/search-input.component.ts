82061a98b3d0700da1906421ae1574af4768200d
unknown <soulayman.al.abdallah@gmail.com>
2021-10-08 22:19:46 +0300

    design edit

--- src/app/componentsOfPages/search-input/search-input.component.ts
+++ src/app/componentsOfPages/search-input/search-input.component.ts
@@ -1,5 +1,8 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { fromEvent } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-search-input',
@@ -8,32 +11,37 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class SearchInputComponent implements OnInit {
 
-  //interact with input
-  searchContent : any = "";
- // @Input() searchContent = "";
+  //interact with input html element
+  searchContent: any = "";
+  
 
-  //apply output
+
+
+  //apply output angular concept with the father: artistSearch page
   @Output() searchContentChanged = new EventEmitter<string>();
- scream() {
-    if (this.searchContent) {
-      this.searchContentChanged.emit(this.searchContent)
-      this._router.navigate(['/artist', this.searchContent])
-    }else{
-    this._router.navigate(['/artist', ''])
-    return}
+  scream() {
+              if (this.searchContent) {
+                this.searchContentChanged.emit(this.searchContent)
+                this._router.navigate(['/artist', this.searchContent])
+              } else {
+                this._router.navigate(['/artist', ''])
+                return
+    }
 
   }
 
 
   constructor(private _router: Router, private route: ActivatedRoute) { }
-
   ngOnInit(): void {
 
-this.searchContent= (this.route.snapshot.paramMap.get('slayquery'))
-this.searchContentChanged.emit(this.searchContent)
+    // applying some RxJS pipes for the API data flow
+let searchBox : any = document.getElementById('searchBox');
+let A01Flux = fromEvent(searchBox,'input').pipe(debounceTime(800), (distinctUntilChanged()))
+A01Flux.subscribe((x)=> {this.scream()}  )
+
+    // Presenting previous search results when browser's back button clicked 
+    this.searchContent = (this.route.snapshot.paramMap.get('slayquery'))
+    this.searchContentChanged.emit(this.searchContent)
   }
-
- 
-
 
 }
